2bc0e721de584612ca09c2a767c8937fb9b6f578
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2021-04-21 21:15:29 -0700

    fix: prevent exceptions when JS is not loaded added tests for when JS is not loaded

--- src/BlazorApplicationInsights.Tests/UnitTests.cs
+++ src/BlazorApplicationInsights.Tests/UnitTests.cs
@@ -25,7 +25,7 @@ namespace BlazorApplicationInsights.Tests
         [InlineData("TrackEvent", false, 2)]
         [InlineData("TrackTrace", false, 4)]
         [InlineData("TrackException", false, 2)]
-        [InlineData("TrackGlobalException", true, 1)]
+        [InlineData("TrackGlobalException", false, 1)]
         [InlineData("SetAuthenticatedUserContext", false, 1)]
         [InlineData("ClearAuthenticatedUserContext", false, 1)]
         [InlineData("StartStopTrackPage", false, 1)]
@@ -47,7 +47,7 @@ namespace BlazorApplicationInsights.Tests
 
             page.Console += (sender, e) =>
             {
-                if (e.Message.Type == "error")
+                if (e.Message.Type == "error" && !e.Message.Text.Contains("Something wrong happened :("))
                 {
                     hasError = true;
                 }
@@ -73,6 +73,61 @@ namespace BlazorApplicationInsights.Tests
 
             hasError.Should().Be(shouldError);
             appInsightsCalls.Should().Be(expectedCalls);
+        }
+
+        [Theory]
+        [InlineData("TrackEvent", false)]
+        [InlineData("TrackTrace", false)]
+        [InlineData("TrackException", false)]
+        [InlineData("TrackGlobalException", false)]
+        [InlineData("SetAuthenticatedUserContext", false)]
+        [InlineData("ClearAuthenticatedUserContext", false)]
+        [InlineData("StartStopTrackPage", false)]
+        [InlineData("TrackDependencyData", false)]
+        [InlineData("TrackMetric", false)]
+        [InlineData("TrackPageView", false)]
+        [InlineData("TrackPageViewPerformance", false)]
+        [InlineData("TestLogger", false)]
+        [InlineData("StartStopTrackEvent", false)]
+        public async Task TestBlocked(string id, bool shouldError)
+        {
+            bool hasError = false;
+
+            using var playwright = await Playwright.CreateAsync();
+
+            await using var browser = await playwright.Chromium.LaunchAsync(Headless);
+            var page = await browser.NewPageAsync();
+
+            page.Console += (sender, e) =>
+            {
+                if (e.Message.Type == "error" && e.Message.Text != "Failed to load resource: net::ERR_FAILED" && !e.Message.Text.Contains("Something wrong happened :("))
+                {
+                    hasError = true;
+                }
+            };
+
+            await page.RouteAsync("https://dc.services.visualstudio.com/v2/track", async (x, y )  => {
+                await x.AbortAsync();
+            });
+
+            await page.RouteAsync("https://js.monitor.azure.com/scripts/b/ai.2.min.js", async (x, y) => {
+                await x.AbortAsync();
+            });
+
+            page.RequestFailed += (sender, e) =>
+            {
+                if (e.Request.Url != "https://js.monitor.azure.com/scripts/b/ai.2.min.js" && e.Request.Url != "https://dc.services.visualstudio.com/v2/track")
+                {
+                    hasError = true;
+                }
+            };
+
+            await page.GoToAsync(BaseAddress);
+            await page.ClickAsync("#" + id);
+
+            await page.WaitForTimeoutAsync(1000);
+
+            hasError.Should().Be(shouldError);
         }
     }
 }
