5dd05aec337e992dbfd03f6c1fae74648cc6124b
Jacob Marks <jacobjmarks@outlook.com>
2024-05-12 18:26:03 +1000

    Provide services to initialization delegate

--- samples/BlazorApplicationInsights.Sample.Wasm/Program.cs
+++ samples/BlazorApplicationInsights.Sample.Wasm/Program.cs
@@ -18,7 +18,7 @@ namespace BlazorApplicationInsights.Sample.Wasm
             {
                 config.ConnectionString = "InstrumentationKey=219f9af4-0842-42c8-a5b1-578f09d2ee27;IngestionEndpoint=https://westus2-0.in.applicationinsights.azure.com/;LiveEndpoint=https://westus2.livediagnostics.monitor.azure.com/";
             },
-            async applicationInsights =>
+            async (_, applicationInsights) =>
             {
                 var telemetryItem = new TelemetryItem()
                 {
