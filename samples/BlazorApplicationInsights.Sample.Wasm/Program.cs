ca7f27567c036de32ee582c1bd4e83ec40eed00f
Ivan Josipovic <9521987+IvanJosipovic@users.noreply.github.com>
2024-05-27 20:29:28 -0700

    fix(wasm): Update guidance due to race condition

--- samples/BlazorApplicationInsights.Sample.Wasm/Program.cs
+++ samples/BlazorApplicationInsights.Sample.Wasm/Program.cs
@@ -14,10 +14,7 @@ namespace BlazorApplicationInsights.Sample.Wasm
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-            builder.Services.AddBlazorApplicationInsights(config =>
-            {
-                config.ConnectionString = "InstrumentationKey=219f9af4-0842-42c8-a5b1-578f09d2ee27;IngestionEndpoint=https://westus2-0.in.applicationinsights.azure.com/;LiveEndpoint=https://westus2.livediagnostics.monitor.azure.com/";
-            },
+            builder.Services.AddBlazorApplicationInsights(null,
             async applicationInsights =>
             {
                 var telemetryItem = new TelemetryItem()
